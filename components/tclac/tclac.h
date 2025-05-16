038cd8f54c95047575348f1cd57edd92eee38b95
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2025-05-16 17:05:18 +0500

    Update tclac.h

--- components/tclac/tclac.h
+++ components/tclac/tclac.h
@@ -36,7 +36,6 @@ namespace tclac {
 #define FAN_MEDIUM      0b10100000  // speed 2
 #define FAN_HIGH        0b11010000  // speed 3
 #define FAN_FOCUS       0b10110000  // speed 4
-#define FAN_TURBO       0b11110000  // turbo (max speed + дополнительный бит)
 #define FAN_DIFFUSE     0b10000000  // POWER [7]
 #define FAN_SPEED_MASK  0b11110000  // FAN SPEED MASK
 
